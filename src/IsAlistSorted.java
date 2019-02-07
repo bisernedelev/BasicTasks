e351ca67b6e25fe706a1f66744e252d54568da61
biserPC <bisernedelev@gmail.com>
2019-02-07 13:47:25 +0200

    Write a program that checks if a list is already sorted. For a list to be sorted, the next element must NOT be smaller than the previous one.

---
+++ src/IsAlistSorted.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class IsAlistSorted {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String n1 = sc.nextLine();
+        int n = Integer.parseInt(n1);
+        String[] result = new String[n];
+
+        int flag = 0;
+
+
+        for (int z = 0; z <n; z++) {
+
+            String input = sc.nextLine();
+            String[] array = input.split(",");
+            int[] arrInt = new int[array.length];
+
+
+            for (int i = 0; i < array.length - 1; i++) {
+                arrInt[i] = Integer.parseInt(array[i]);
+                arrInt[i + 1] = Integer.parseInt(array[i + 1]);
+            }
+            for (int y = 0; y <= arrInt.length - 2; y++) {
+                if (arrInt[y] > arrInt[y + 1]) {
+                    flag = 1;
+                }
+            }
+            if (flag == 1) {
+                result[z]="false";
+            } else {
+                result[z]="true";
+            }
+            flag=0;
+        }
+        for(int a =0;a<result.length;a++){
+            System.out.println(result[a]);
+        }
+
+    }
+}
