e0badcc849fbfa5b4d746330802ac0d3fb09297b
biserPC <bisernedelev@gmail.com>
2019-02-08 10:26:54 +0200

    Write a program that reads a list of numbers separated by a comma and space.
    Arrange the numbers in descending order.
    
    Output all numbers on a single line, separated by a comma and a space.

---
+++ AllTasks/SortNumbers.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortNumbers {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String[] arrStr = input.split(", ");
+        int[] arrInt = new int[arrStr.length];
+
+        for (int i = 0; i <= arrInt.length - 1; i++) {
+            arrInt[i] = Integer.parseInt(arrStr[i]);
+        }
+        Arrays.sort(arrInt);
+        for (int y = arrInt.length - 1; y >= 1; y--)
+            System.out.print(arrInt[y] + ", ");
+        System.out.print(arrInt[0]);
+
+    }
+}
