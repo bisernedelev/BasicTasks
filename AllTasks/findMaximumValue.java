cbf9992155a0b94e09cdfeca3638b5eea08fa798
biserPC <bisernedelev@gmail.com>
2019-02-09 10:29:24 +0200

    Write a program that finds the maximum number from a given sequence.

---
+++ AllTasks/findMaximumValue.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class findMaximumValue {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int max = sc.nextInt();
+
+        for (int i = 1; i <= n - 1; i++) {
+            int a = sc.nextInt();
+            if (a > max) {
+                max = a;
+            }
+
+
+        }
+        System.out.println(max);
+    }
+}
