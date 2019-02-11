3e47efe5c2fefd9f4f0fc8ccbd0f6a9223ea58aa
biserPC <bisernedelev@gmail.com>
2019-02-11 12:24:23 +0200

    Write a program that, for a given two numbers N and x, calculates the sum below.
    S=1+1!x+2!x2+⋯+N!xN
    Use only one loop. Print the result with 5 digits after the decimal point.

---
+++ AllTasks/CalculateComplexSum.java
@@ -0,0 +1,30 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class CalculateComplexSum {
+    public static void main(String[] args) {
+
+        DecimalFormat df = new DecimalFormat("0.00000");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+
+        double a = 1;
+        double total = 1;
+
+        for (int i = 1; i <= n; i++) {
+            int b = 0;
+            double integral = (a * (a - (a - 1)));
+            double sum = integral / (Math.pow(x, i));
+            a = a * (i + 1);
+
+            total += sum;
+
+
+
+        }
+
+        System.out.println(df.format(total));
+
+    }
+}
