45862a277701452f996a5e858bbc0f60430c6b84
biserPC <bisernedelev@gmail.com>
2019-02-11 12:25:44 +0200

    You need to provide conversion of the temperature given in Celsius to their Fahrenheit representation.

---
+++ AllTasks/ConvertDegrees.java
@@ -0,0 +1,21 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class ConvertDegrees {
+    public static void main(String[] args) {
+
+        DecimalFormat df = new DecimalFormat("#.00");
+        Scanner sc = new Scanner(System.in);
+
+
+        while(sc.hasNext()){
+            int i = sc.nextInt();
+            int a= (int) ((i*1.8)+32);
+            System.out.println(a);
+        }
+
+
+
+
+    }
+}
