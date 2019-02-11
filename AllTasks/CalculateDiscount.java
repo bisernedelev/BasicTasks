1e6e3079a6c55dbcdf06ff8d555236825bab9947
biserPC <bisernedelev@gmail.com>
2019-02-11 12:26:46 +0200

    You need to calculate the discounted price for each item in your shopping cart. The discount is 65%, a real deal for you.

---
+++ AllTasks/CalculateDiscount.java
@@ -0,0 +1,21 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class CalculateDiscount {
+    public static void main(String[] args) {
+
+        DecimalFormat df = new DecimalFormat("#.00");
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble();
+        double number;
+        double total = 0;
+
+        for (int i = 0; i < x; i++) {
+            number = sc.nextDouble();
+            total = number - (number * 0.65);
+            System.out.println(df.format(total));
+        }
+
+
+    }
+}
