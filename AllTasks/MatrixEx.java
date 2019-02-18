25b9589c366e094d8c9ea65d9fece42d168cc4e1
biserPC <bisernedelev@gmail.com>
2019-02-18 09:42:01 +0200

    Filling a matrix

---
+++ AllTasks/MatrixEx.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class MatrixEx {
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int count = sc.nextInt();
+
+        int[][] m1 = new int[n][n];
+        for (int row = 0; row < m1.length; row++) {
+            for (int col = 0; col < m1[row].length; col++) {
+                m1[row][col] = count;
+                count++;
+                System.out.print(String.format("%d ", m1[row][col]));
+            }
+
+            System.out.println();
+
+        }
+
+
+    }
+
+}
