5df910baf19a4e99f94735e5a55489d6097654fd
Biser Nedelev <bisernedelev@gmail.com>
2019-06-01 20:05:06 +0300

    Update CheckCapitalLetters.java

--- AllTasks/CheckCapitalLetters.java
+++ AllTasks/CheckCapitalLetters.java
@@ -27,7 +27,6 @@ public class CheckCapitalLetters {
 
         ArrayList<String> words1 = new ArrayList<String>();
 
-
         int counter = 0;
         for (int i = 0; i < test1.length; i++) {
             if (checkCapitalLetter(test1[i])){
@@ -42,7 +41,6 @@ public class CheckCapitalLetters {
 
 
          }
-
         System.out.println(test1.length);
         System.out.println(Arrays.toString(test1));
         System.out.println(counter);
