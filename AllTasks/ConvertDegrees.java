905878889834e6c7e67bfb803311d01a0ecdeeb3
Biser Nedelev <bibo0088@abv.bg>
2019-07-10 01:07:14 +0300

    Update ConvertDegrees.java

--- AllTasks/ConvertDegrees.java
+++ AllTasks/ConvertDegrees.java
@@ -6,9 +6,7 @@ public class ConvertDegrees {
 
         DecimalFormat df = new DecimalFormat("#.00");
         Scanner sc = new Scanner(System.in);
-
-
-        while(sc.hasNext()){
+            while(sc.hasNext()){
             int i = sc.nextInt();
             int a= (int) ((i*1.8)+32);
             System.out.println(a);
