485bb44b98ac3c5d0cc40d88131ae25ddc9f16c8
Biser Nedelev <bibo0088@abv.bg>
2019-08-19 14:27:01 +0300

    Update CombineList.java

--- AllTasks/CombineList.java
+++ AllTasks/CombineList.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class CombineList {
     public static void main(String[] args) {
 
+        
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         String input2 = sc.nextLine();
